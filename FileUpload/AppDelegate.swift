917cd14dc698d66b080d6627a0f81a8be1169497
Frank Tisellano <frank@frank.is>
2015-04-14 15:22:08 -0400

    Initial Commit

---
+++ FileUpload/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileUpload
+//
+//  Created by Frank Tisellano on 4/14/15.
+//  Copyright (c) 2015 Frank Tisellano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileUpload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileUpload
+//
+//  Created by Frank Tisellano on 4/14/15.
+//  Copyright (c) 2015 Frank Tisellano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
